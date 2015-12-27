8beb27d25e5f312091b51ce4eb44c63b41afe75a
Nathan Kozyra <nkozyra@gmail.com>
2015-12-26 23:46:55 -0500

    hm

---
+++ elysium.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"log"
+)
+
+const (
+	VERSION = "0.01"
+)
+
+func init() {
+	log.Println("\360\237\215\224 \tElysium v" + VERSION)
+}
+
+func main() {
+
+}
---
+++ tools/dev.data.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"log"
+)
+
+const (
+	DATALOC = "s3://"
+)
+
+func Migrate() bool {
+
+	return true
+}
+
+func main() {
+	log.Println("Oy")
+}
