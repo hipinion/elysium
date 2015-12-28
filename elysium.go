738dddddfb57f33923b0e2be04640bd74397ebb4
Nathan Kozyra <nkozyra@gmail.com>
2015-12-27 23:06:37 -0500

    Increments

--- elysium.go
+++ elysium.go
@@ -1,12 +1,27 @@
 package main
 
 import (
+	"io/ioutil"
 	"log"
+	"os"
 )
 
 const (
 	VERSION = "0.01"
+	CONFIG  = "config/config.json"
 )
+
+type Config struct {
+}
+
+func loadConfig() {
+	cfile, err := os.Open(CONFIG)
+	if err != nil {
+		log.Fatalln("Could not open config", CONFIG)
+	}
+	config, err := ioutil.ReadAll(cfile)
+	log.Println(config)
+}
 
 func init() {
 	log.Println("\360\237\215\224 \tElysium v" + VERSION)
