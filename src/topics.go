ba10f4dbcb3fc79c6b1b7ef05f5f0e057ab3676d
Nathan Kozyra <nkozyra@gmail.com>
2016-02-08 12:26:15 -0500

    allows bootstrap content to display

--- src/topics.go
+++ src/topics.go
@@ -14,11 +14,27 @@ type Topic struct {
 	Posts []Post `json:"posts"`
 }
 
-func GetTopic(guid string) Topic {
-	t := Topic{}
-	err := DB.QueryRow("SELECT topic_guid, topic_title FROM topics WHERE topic_guid=?", guid).Scan(&t.Guid, &t.Title)
+func (t Topic) GetPosts() []Post {
+
+	rows, err := DB.Query("SELECT post_id, post_text, user_id FROM posts WHERE topic_id=?", t.ID)
 	if err != nil {
 		log.Println(err)
 	}
+	for rows.Next() {
+		p := Post{}
+		rows.Scan(&p.ID, &p.Text, &p.User)
+		t.Posts = append(t.Posts, p)
+	}
+	return t.Posts
+}
+
+func GetTopic(guid string) Topic {
+	t := Topic{}
+	err := DB.QueryRow("SELECT topic_id, topic_guid, topic_title FROM topics WHERE topic_guid=?", guid).Scan(&t.ID, &t.Guid, &t.Title)
+	if err != nil {
+		log.Println(err)
+	}
+	t.Posts = t.GetPosts()
+
 	return t
 }
